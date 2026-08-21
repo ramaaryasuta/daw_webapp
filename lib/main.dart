b482772c4c8490412f006ee5f49dd657762822d6
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2026-08-21 18:50:44 +0700

    feat : setup firebase hosting

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:daw_webapp/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
 
   runApp(const ProviderScope(child: DawApp()));
 }
